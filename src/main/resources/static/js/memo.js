b51fe65e4d007348f4d2f752b9dcb0ed2fbface7
yamadasynapse <tyamada@synapse.jp>
2025-05-29 09:32:53 +0900

    コントローラーにリクエストを送信する

--- src/main/resources/static/js/memo.js
+++ src/main/resources/static/js/memo.js
@@ -1,5 +1,15 @@
 function post (){
-  console.log("イベント発火");
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+
+  });
 };
 
 window.addEventListener('load', post);
